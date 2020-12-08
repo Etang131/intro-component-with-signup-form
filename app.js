83e9d8d8ece5238cfa321124037d687ba96654bd
Erik Tangvik <erikjt@outlook.com>
2020-12-08 16:03:01 -0600

    added vars

---
+++ app.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("form");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  checkInputs() {
+    // Get value from inputs
+    firstName
+    lastName
+    email
+    password
+  }
+})
