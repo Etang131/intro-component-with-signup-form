4bc5a6ce3dc4f4ac33ca2cc0df4fdb147ae9c16f
Erik Tangvik <erikjt@outlook.com>
2020-12-09 08:48:47 -0600

    added validation for form

--- app.js
+++ app.js
@@ -4,14 +4,66 @@ const lastName = document.getElementById("lastName");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  checkInputs() {
-    // Get value from inputs
-    firstName
-    lastName
-    email
-    password
+  checkInputs();
+});
+
+function checkInputs() {
+  // Get value from inputs and trim values
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (firstNameValue === "") {
+    //show error
+    // add error class
+    setErrorFor(firstName, "First Name cannot be empty");
+  } else {
+    setSuccessFor(firstName);
   }
-})
+
+  if (lastNameValue === "") {
+    setErrorFor(lastName, "Last Name cannot be empty");
+  } else {
+    setSuccessFor(lastName);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be empty");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Looks like this is not an email");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be empty");
+  } else {
+    setSuccessFor(password);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formBlock = input.parentElement; // .formBlock
+  const small = formBlock.querySelector("small");
+
+  // add error message inside small tag
+  small.innerText = message;
+
+  //add error class
+  formBlock.className = "formBlock failure";
+}
+
+function setSuccessFor(input) {
+  const formBlock = input.parentElement;
+  formBlock.className = "formBlock";
+}
+
+function isEmail(email) {
+  return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
+    email
+  );
+}
